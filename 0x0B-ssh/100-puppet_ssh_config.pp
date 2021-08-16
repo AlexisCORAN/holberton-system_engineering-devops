96e42eb1cfbcc57afe3e7496306b30e7d9d8ec6e
AlexisCORAN <alexis_ca_@outlook.com>
2021-08-16 01:52:17 +0000

    Added Advanced tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # ssh configuration
-file { 'Turn off passwd auth':
+file_line { 'Turn off passwd auth':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
 
-file { 'Declare identity file':
+file_line { 'Declare identity file':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/holberton',
 }
