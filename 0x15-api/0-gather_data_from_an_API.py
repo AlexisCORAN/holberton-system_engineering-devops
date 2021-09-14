f951bf107fde6a44f89806e6412ce0c9d07e6959
Alexis Coronado <AlexisCORAN@users.noreply.github.com>
2021-09-13 23:41:06 -0500

    Fixed pep8 style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,22 +17,23 @@ if __name__ == '__main__':
     final_url = '{}/todos'.format(user_url)
 
     response_user = requests.get(user_url).json()
-    
+
     name = response_user.get('name')
-    
+
     response_final = requests.get(final_url).json()
-    
+
     total = len(response_final)
-    
+
     completed = 0
     tasks = []
-    
+
     for task in response_final:
         if task.get('completed'):
             completed += 1
             tasks.append(task)
-    
+
     print('Employee {} is done with tasks({}/{}):'
           .format(name, completed, total))
+
     for task in tasks:
         print('\t', task.get('title'))
