0f9d1b69251740def6ed1025d742f3d38b27f11a
AlexisCORAN <alexis_ca_@outlook.com>
2021-09-15 20:03:39 +0000

    Updated tasks

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,27 +9,21 @@ import sys
 if __name__ == '__main__':
 
     api_url = 'https://jsonplaceholder.typicode.com'
-    users_url = '{}/users'.format(api_url)
-    todos_url = '{}/todos'.format(api_url)
-
-    response_user = requests.get(users_url).json()
-
-    response_final = requests.get(todos_url).json()
-
+    user_url = '{}/users'.format(api_url)
+    response_users = requests.get(user_url).json()
     users_tasks = {}
-    tasks = []
-    temp = {}
 
-    for user in response_user:
+    for user in response_users:
         user_id = user.get('id')
-        username = user.get('username')
+        users_tasks[user_id] = []
+        todos_url = '{}/{}/todos'.format(user_url, user_id)
+        response_final = requests.get(todos_url).json()
         for task in response_final:
-            temp['username'] = username
-            temp['task'] = task['title']
-            temp['completed'] = task['completed']
-            tasks.append(temp)
+            task = {'task': task.get('title'),
+                    'username': user.get('username'),
+                    'completed': task.get('completed')}
+            users_tasks[user_id].append(task)
 
-        users_tasks[user_id] = tasks
 
     with open('todo_all_employees.json', 'w', encoding='utf-8') as json_file:
         json.dump(users_tasks, json_file)
