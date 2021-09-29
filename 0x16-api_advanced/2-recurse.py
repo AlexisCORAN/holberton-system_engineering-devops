687d9e25e02bfc988990514c1e68bb6bd8999da0
AlexisCORAN <alexis_ca_@outlook.com>
2021-09-29 04:43:07 +0000

    Added tasks

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module defines the recurse function
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    param = {"after": after, "limit": "100"}
+    user_agent = {"User-Agent": "Python"}
+
+    response = requests.get(url, headers=user_agent, params=param, allow_redirects=False)
+
+    if response.status_code >= 400:
+        return None
+
+    response = response.json()
+    hot_posts = response.get("data").get("children")
+    after = response.get("data").get("after")
+
+    for post in hot_posts:
+        hot_list.append(post.get("data").get("title"))
+
+    if after is not None:
+        recurse(subreddit, hot_list, after)
+    
+    return hot_list
