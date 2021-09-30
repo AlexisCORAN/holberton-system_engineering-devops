c54f59d0f33ef4f76b9cfee6642d50adefb3fc08
AlexisCORAN <alexis_ca_@outlook.com>
2021-09-30 03:24:54 +0000

    Fixed syntax

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Script Fix Wordpress
 exec { 'Fix-Wordpress':
-  path => '/usr/bin',
+  path     => '/usr/bin',
   provider => shell,
-  command => 'sudo sed -i s/class-wp-locale.phpp/class-wp-locale.php/g /var/www/html/wp-settings.php'
+  command  => 'sudo sed -i s/class-wp-locale.phpp/class-wp-locale.php/g /var/www/html/wp-settings.php'
 }
